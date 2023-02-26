a105e1f30b3975f76ba16360a1e76037a9234729
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-26 18:30:21 +0100

    next draw section + fetching data from thirdweb

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,16 +8,37 @@ import {
   useMetamask,
   useDisconnect,
   useContractMetadata,
-  useContractCall,
+  useContractRead,
 } from "@thirdweb-dev/react";
 import Login from "../components/Login";
 import Loading from "../components/Loading";
+import { useState } from "react";
+import { ethers } from "ethers";
+import { currency } from "../constants";
 
 const Home: NextPage = () => {
+  const [quantity, setQuantity] = useState<number>(1);
+
+  // thirdweb hooks
   const address = useAddress();
   const { contract, isLoading } = useContract(
     process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
   );
+  const { data: remainingTickets } = useContractRead(
+    contract,
+    "RemainingTickets"
+  );
+  const { data: currentWinningReward } = useContractRead(
+    contract,
+    "CurrentWinningReward"
+  );
+  const { data: ticketPrice } = useContractRead(contract, "ticketPrice");
+  const { data: ticketCommission } = useContractRead(
+    contract,
+    "ticketCommission"
+  );
+  const { data: expiration } = useContractRead(contract, "expiration");
+  // ---
 
   if (isLoading) return <Loading />;
 
@@ -30,8 +51,106 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-      <h1>Essa</h1>
+      <div className="flex-1">
+        <Header />
+
+        {/* NEXT DRAW BOX */}
+        <div className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5">
+          <div className="stats-container">
+            <h1 className="text-5xl text-white font-semibold text-center">
+              The Next Draw
+            </h1>
+            <div className="flex justify-between p-2 space-x-2">
+              <div className="stats">
+                <h2 className="text-sm">Total Pool:</h2>
+                <p className="text-xl">
+                  {currentWinningReward &&
+                    ethers.utils.formatEther(
+                      currentWinningReward.toString()
+                    )}{" "}
+                  {currency}
+                </p>
+              </div>
+              <div className="stats">
+                <h2 className="text-sm">Tickets Remaining:</h2>
+                <p className="text-xl">{remainingTickets?.toNumber()}</p>
+              </div>
+            </div>
+
+            {/* Countdown Timer */}
+            {/* ... */}
+          </div>
+
+          {/* PRICE TICKET BOX */}
+          <div className="stats-container space-y-2">
+            <div className="stats-container">
+              <div className="flex justify-between items-center text-white pb-2">
+                <h2>Price per ticket:</h2>
+                <p>
+                  {ticketPrice &&
+                    ethers.utils.formatEther(ticketPrice.toString())}{" "}
+                  {currency}
+                </p>
+              </div>
+
+              <div className="flex text-white items-center space-x-2 bg-[#091B18] border-[#004337] border p-4">
+                <p>TICKETS:</p>
+                <input
+                  type="number"
+                  name=""
+                  id=""
+                  className="flex w-full bg-transparent text-right outline-none"
+                  min={1}
+                  max={20}
+                  value={quantity}
+                  onChange={(e) => setQuantity(Number(e.target.value))}
+                />
+              </div>
+
+              <div className="space-y-2 mt-5">
+                <div className="flex items-center justify-between text-emerald-300 text-sm italic font-extrabold">
+                  <p>Total cost of tickets</p>
+                  <p>
+                    {ticketPrice &&
+                      Number(ethers.utils.formatEther(ticketPrice.toString())) *
+                        quantity}{" "}
+                    {currency}
+                  </p>
+                </div>
+
+                <div className="flex items-center justify-between text-emerald-300 text-xs italic">
+                  <p>Service fees</p>
+                  <p>
+                    {ticketCommission &&
+                      ethers.utils.formatEther(
+                        ticketCommission.toString()
+                      )}{" "}
+                    {currency}
+                  </p>
+                </div>
+
+                <div className="flex items-center justify-between text-emerald-300 text-xs italic">
+                  <p>+ Network Fees</p>
+                  <p>TBC</p>
+                </div>
+              </div>
+
+              <button
+                disabled={
+                  expiration?.toString() < Date.now().toString() ||
+                  remainingTickets?.toNumber() === 0
+                }
+                className="mt-5 w-full bg-gradient-to-br from-orange-500 
+          to-emerald-600 px-10 py-5 rounded-md 
+          text-white shadow-xl disabled:from-gray-600 
+          disabled:to-gray-800 disabled:cursor-not-allowed text-xl"
+              >
+                Buy tickets
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
